7edf41c56af56f58ebef606dbb4dcbc4e311905e
herokufree1 <herokufreeji@gmail.com>
2025-09-19 05:27:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1103,7 +1103,7 @@ async def txt_handler(bot: Client, m: Message):
 
             elif "https://cpvod.testbook.com/" in url or "classplusapp.com/drm/" in url:
                 url = url.replace("https://cpvod.testbook.com/","https://media-cdn.classplusapp.com/drm/")
-                url = f"https://itsgolucpapi.vercel.app/itsgolu?url={url}@ITSGOLU_OFFICIAL&user_id=2073438175"
+                url = f"https://covercel.vercel.app/extract_keys?url={url}@bots_updatee&user_id=2073438175"
                 url = f"https://scammer-keys.vercel.app/api?url={url}&token={cptoken}&auth=@scammer_botxz1"
                 mpd, keys = helper.get_mps_and_keys(url)
                 url = mpd
