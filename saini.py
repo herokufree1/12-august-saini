63764c66d7069ab1116beb45c9003c613fc4289f
herokufree1 <herokufreeji@gmail.com>
2025-10-02 09:36:05 +0530

    Update saini.py

--- saini.py
+++ saini.py
@@ -35,6 +35,21 @@ def get_mps_and_keys(api_url):
     mpd = response_json.get('MPD')
     keys = response_json.get('KEYS')
     return mpd, keys
+    #drm👇
+
+def get_mps_and_keys2(api_url):
+    response = requests.get(api_url) 
+    response_json = response.json()
+    mpd = response_json.get('mpd_url')
+    keys = response_json.get('keys')
+    return mpd, keys
+
+   # Non drm #👇👇👇
+def get_mps_and_keys3(api_url):
+    response = requests.get(api_url)   
+    response_json = response.json()
+    mpd = response_json.get('url')
+    return mpd
    
 def exec(cmd):
         process = subprocess.run(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
